9e84f751d4721e43086fc546560f9892cd38356a
Xica Felix <xicasfelix@gmail.com>
2022-07-28 12:43:39 -0400

    Wiring Submit Form to Filter Through Census Data

--- index.js
+++ index.js
@@ -16,11 +16,37 @@ dropDown.addEventListener('change', (event)=>{
     event.preventDefault();
     console.log(event.target.value);
     dropDownValue = event.target.value;
-    fetch(`https://api.census.gov/data/2019/acs/flows?get=FULL1_NAME,FULL2_NAME,MOVEDIN,MOVEDOUT,MOVEDNET,GEOID1,GEOID2&for=county:${dropDownValue}&in=state:36`)
+    fetch(`https://api.census.gov/data/2019/acs/flows?get=FULL2_NAME,MOVEDIN,MOVEDOUT,MOVEDNET,GEOID1,GEOID2&for=county:${dropDownValue}&in=state:36`)
     .then(resp=>resp.json())
     .then(data=>renderFlows(data));
 })
 
+let censusData = [];
+
 function renderFlows(data){
     console.log(data.slice(0,10));
+    censusData = data.slice(1);
+    // console.log(`Census Data: ${censusData[0]}`);
+    return censusData;
 }
+
+
+
+// Adding Event Listener to the Submit Form
+
+
+const submitForm = document.getElementById('county-form');
+submitForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    // console.log(event.target[0].value);
+   let countyName = String(event.target[0].value);
+//    console.log(censusData);
+
+    censusData.forEach((item)=>{
+        let county = item[0];
+        if (item[0]=== countyName) {
+            console.log(item);
+        }
+    })
+   
+})
